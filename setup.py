dfdab5cb3377ef8b7b05b83f3fec7ca5f49a5060
Ayoub Kaanich <kayoub5@live.com>
2018-11-14 10:26:26 +0100

    Update dependency future to >=0.15.2, <0.18

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ setup(
         'statsd>=3.2.1,<4.0',
         'requests>=2.10.0,<3.0',
         'raven>=5.27.1,<7.0',
-        'future>=0.15.2,<0.17',
+        'future>=0.15.2, <0.18',
         'ipaddress>=1.0.16,<2.0;python_version<"3.3"',
     ],
     keywords=[
